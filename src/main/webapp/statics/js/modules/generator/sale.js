b894554d6f0e8be03d967747d9533bdc5cc811bc
payne <1802290848@qq.com>
2018-03-13 23:03:19 +0800

    sale.html编写

--- src/main/webapp/statics/js/modules/generator/sale.js
+++ src/main/webapp/statics/js/modules/generator/sale.js
@@ -2,22 +2,32 @@ $(function () {
     $.get(baseURL + "crm/sale/typelist", function(r){
         vm.types = r.type;
     });
+    $.get(baseURL + "crm/sale/clueslist?typeid="+1
 
+
+
+        , function(r){
+        // console.log(r);
+        vm.clues = r.clues;
+    });
 })
 
 var vm = new Vue({
     el:'#sales',
     data: {
         types:{},
-        activeId: 1
+        activeId: 1,
+        clues:{}
     },
     methods:{
         getClues:function (e) {
             vm.activeId = e.target.id;
             var typeid = e.target.id
             $.get(baseURL + "crm/sale/clueslist?typeid="+typeid, function(r){
-                console.log(r);
+                // console.log(r);
+                vm.clues = r.clues;
             });
+
         }
     },
     created:function () {
