31d23f467fc8f63375933f1756fbbd920cb0571a
payne <1802290848@qq.com>
2018-03-19 21:38:02 +0800

    样式修改

--- src/main/webapp/statics/js/modules/generator/sale.js
+++ src/main/webapp/statics/js/modules/generator/sale.js
@@ -9,7 +9,7 @@ var app = new Vue({
         customers:{},
         products:{},
         cname:"",
-        pname: ""
+        pname: "",
     },
     methods:{
 
@@ -17,44 +17,40 @@ var app = new Vue({
             app.activeId = e.target.id;
             var typeid = e.target.id
             $.get(baseURL + "crm/sale/clueslist?typeid="+typeid, function(r){
-                console.log(r);
+                // console.log(r);
                 app.clues = r.clue;
             });
         },
         add:function () {
             $.get(baseURL + "crm/customer/customerlist", function(r){
                 app.customers = r.customerList;
-                console.log(app.customers);
+                // console.log(app.customers);
             });
             $.get(baseURL + "crm/product/productlist", function(r){
                 app.products = r.productList;
-                console.log(app.products);
+                // console.log(app.products);
             });
             app.showList=false;
         },
         back:function () {
             app.showList=true;
+        },
+        addClues:function(e){
+            console.log(e)
         }
+
     },
     created:function () {
         $.get(baseURL + "crm/sale/typelist", function(r){
             app.types = r.type;
         });
         $.get(baseURL + "crm/sale/clueslist?typeid="+1, function(r){
-            console.log(r);
+            // console.log(r);
             app.clues = r.clue;
         });
         $.getJSON("sys/user/info?_"+$.now(), function(r){
             app.user = r.user;
-            console.log(app.user)
-        });
-        $.get(baseURL + "crm/customer/customerlist", function(r){
-            app.customers = r.customerList;
-            console.log(app.customers);
-        });
-        $.get(baseURL + "crm/product/productlist", function(r){
-            app.products = r.productList;
-            console.log(app.products);
+            // console.log(app.user)
         });
     }
 })
